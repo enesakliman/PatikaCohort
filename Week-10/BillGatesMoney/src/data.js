be7a84af7c8e52a83e7cf753047af970ee7835b0
MaedreDergau <161196056+MaedreDergau@users.noreply.github.com>
2025-01-22 14:49:59 +0300

    Bill Gates Start

---
+++ Week-10/BillGatesMoney/src/data.js
@@ -0,0 +1,93 @@
+import BigMac from "./assets/big-mac.jpg";
+import FlipFlops from "./assets/flip-flops.jpg";
+import CocaColaPack from "./assets/coca-cola-pack.jpg";
+import MovieTicket from "./assets/movie-ticket.jpg";
+import Book from "./assets/book.jpg";
+import LobsterDinner from "./assets/lobster-dinner.jpg";
+import VideoGame from "./assets/video-game.jpg";
+import AmazonEcho from "./assets/amazon-echo.jpg";
+import YearOfNetflix from "./assets/year-of-netflix.jpg";
+import AirJordans from "./assets/air-jordans.jpg";
+import Airpods from "./assets/airpods.jpg";
+import GamingConsole from "./assets/gaming-console.jpg";
+import Drone from "./assets/drone.jpg";
+import Smartphone from "./assets/smartphone.jpg";
+import Bike from "./assets/bike.jpg";
+import Kitten from "./assets/kitten.jpg";
+import Puppy from "./assets/puppy.jpg";
+import AutoRickshaw from "./assets/auto-rickshaw.jpg";
+import Horse from "./assets/horse.jpg";
+import AcreOfFarmland from "./assets/acre-of-farmland.jpg";
+import DesignerHandbag from "./assets/designer-handbag.jpg";
+import HotTub from "./assets/hot-tub.jpg";
+import LuxuryWine from "./assets/luxury-wine.jpg";
+import DiamondRing from "./assets/diamond-ring.jpg";
+import JetSki from "./assets/jet-ski.jpg";
+import Rolex from "./assets/rolex.jpg";
+import FordF150 from "./assets/ford-f-150.jpg";
+import Tesla from "./assets/tesla.jpg";
+import MonsterTruck from "./assets/monster-truck.jpg";
+import Ferrari from "./assets/ferrari.jpg";
+import SingleFamilyHome from "./assets/single-family-home.jpg";
+import GoldBar from "./assets/gold-bar.jpg";
+import McDonaldsFranchise from "./assets/mcdonalds-franchise.jpg";
+import SuperbowlAd from "./assets/superbowl-ad.jpg";
+import Yacht from "./assets/yacht.jpg";
+import M1Abrams from "./assets/m1-abrams.jpg";
+import Formula1Car from "./assets/formula-1-car.jpg"; 
+import ApacheHelicopter from "./assets/apache-helicopter.jpg";
+import Mansion from "./assets/mansion.jpg";
+import MakeAMovie from "./assets/make-a-movie.jpg";
+import Boeing747 from "./assets/boeing-747.jpg";
+import MonaLisa from "./assets/mona-lisa.jpg";
+import Skyscraper from "./assets/skyscraper.jpg";
+import CruiseShip from "./assets/cruise-ship.jpg";
+import NBATeam from "./assets/nba-team.jpg";
+
+export const DATA = [
+  { id: 1, title: "Big Mac", price: 2, image: BigMac },
+  { id: 2, title: "Flip Flops", price: 3, image: FlipFlops },
+  { id: 3, title: "Coca-Cola Pack", price: 5, image: CocaColaPack,},
+  { id: 4, title: "Movie Ticket", price: 12, image: MovieTicket,},
+  { id: 5, title: "Book", price: 15, image: Book },
+  { id: 6, title: "Lobster Dinner", price: 45, image: LobsterDinner,},
+  { id: 7, title: "Video Game", price: 60, image: VideoGame, },
+  { id: 8, title: "Amazon Echo", price: 99, image: AmazonEcho, },
+  { id: 9, title: "Year of Netflix", price: 100, image: YearOfNetflix,},
+  { id: 10, title: "Air Jordans", price: 125, image: AirJordans, },
+  { id: 11, title: "Airpods", price: 199, image: Airpods },
+  { id: 12, title: "Gaming Console", price: 299, image: GamingConsole, },
+  { id: 13, title: "Drone", price: 350, image: Drone, },
+  { id: 14, title: "Smartphone", price: 699, image: Smartphone, },
+  { id: 15, title: "Bike", price: 800, image: Bike },
+  { id: 16, title: "Kitten", price: 1500, image: Kitten },
+  { id: 17, title: "Puppy", price: 1500, image: Puppy },
+  { id: 18, title: "Auto Rickshaw", price: 2300, image: AutoRickshaw, },
+  { id: 19, title: "Horse", price: 2500, image: Horse },
+  { id: 20, title: "Acre of Farmland", price: 3000, image: AcreOfFarmland, },
+  { id: 21, title: "Designer Handbag", price: 5500, image: DesignerHandbag,},
+  { id: 22, title: "Hot Tub", price: 6000, image: HotTub },
+  { id: 23, title: "Luxury Wine", price: 7000, image: LuxuryWine,},
+  { id: 24, title: "Diamond Ring", price: 10000, image: DiamondRing},
+  { id: 25, title: "Jet Ski", price: 12000, image: JetSki },
+  { id: 26, title: "Rolex", price: 15000, image: Rolex },
+  { id: 27, title: "Ford F-150", price: 30000, image: FordF150,},
+  { id: 28, title: "Tesla", price: 75000, image: Tesla },
+  { id: 29, title: "Monster Truck", price: 150000, image: MonsterTruck,},
+  { id: 30, title: "Ferrari", price: 250000, image: Ferrari },
+  { id: 31, title: "Single Family Home", price: 300000, image: SingleFamilyHome,},
+  { id: 32, title: "Gold Bar", price: 700000, image: GoldBar },
+  { id: 33, title: "McDonald's Franchise", price: 1500000, image: McDonaldsFranchise,},
+  { id: 34, title: "Superbowl Ad", price: 5250000, image: SuperbowlAd,},
+  { id: 35, title: "Yacht", price: 7500000, image: Yacht },
+  { id: 36, title: "M1 Abrams", price: 8000000, image: M1Abrams,},
+  { id: 37, title: "Formula 1 Car", price: 15000000, image: Formula1Car, },
+  { id: 38, title: "Apache Helicopter", price: 31000000, image: ApacheHelicopter,},
+  { id: 39, title: "Mansion", price: 45000000, image: Mansion },
+  { id: 40, title: "Make a Movie", price: 100000000, image: MakeAMovie,},
+  { id: 41, title: "Boeing 747", price: 148000000, image: Boeing747,},
+  { id: 42, title: "Mona Lisa", price: 780000000, image: MonaLisa,},
+  { id: 43, title: "Skyscraper", price: 850000000, image: Skyscraper,},
+  { id: 44, title: "Cruise Ship", price: 930000000, image: CruiseShip,},
+  { id: 45, title: "NBA Team", price: 2120000000, image: NBATeam,},
+];
