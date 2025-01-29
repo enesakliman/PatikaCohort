90c3c3d75945ae537316201372e11a2e449f6ccb
MaedreDergau <161196056+MaedreDergau@users.noreply.github.com>
2025-01-29 17:13:02 +0300

    Genel görüntü oluşturuldu.

--- Week-10/NoteApp/src/App.jsx
+++ Week-10/NoteApp/src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='notes-app-container'>
+        <div>
+          <h1>NotesApp</h1>
+          <input type="search" placeholder='Search'/>
+          <div>
+            <textarea name="notes-input" id=""></textarea>
+            <div className='colors-input'>
+              <input type="checkbox" name="pink" id="pink" />
+              <input type="checkbox" name="purple" id="purple" />
+              <input type="checkbox" name="yellow" id="yellow" />
+              <input type="checkbox" name="blue" id="blue" />
+              <input type="checkbox" name="green" id="green" />
+            </div>
+            <button>Add</button>
+          </div>
+        </div>
+        <div className='notes-container'>
+          Notes
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
