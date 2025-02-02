8b6800dd6254540f66b5e74de02019aab26308c9
MaedreDergau <161196056+MaedreDergau@users.noreply.github.com>
2025-02-02 15:21:14 +0300

    Girdi alındı ve işlenmeye hazır

--- Week-10/NoteApp/src/App.jsx
+++ Week-10/NoteApp/src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
+import { useNote } from "./context/NoteContext";
 
 function App() {
+  const { handleAddNote, handleChange, handleColorChange, note, color, colors } =
+    useNote();
   return (
     <>
       <div className="notes-app-container">
@@ -8,31 +11,32 @@ function App() {
           <h1>NotesApp</h1>
           <input className="search" type="search" placeholder="Search" />
           <div className="notes-input">
-            <textarea name="notes-input" rows={17} cols={10} placeholder="Enter your note here..."></textarea>
+            <textarea
+              onChange={handleChange}
+              name="notes-input"
+              rows={17}
+              cols={10}
+              placeholder="Enter your note here..."
+              value={note}
+                  
+            ></textarea>
             <div className="notes-input-footer">
               <div className="colors-input">
-                <label className="color-label" style={{ backgroundColor: "#f36795" }}>
-                  <input type="radio" name="color" value="pink" />
+              {colors.map((clr) => (
+                <label key={clr.hex} className="color-label" style={{ backgroundColor: clr.hex }}>
+                  <input
+                    type="radio"
+                    name="color"
+                    value={clr.name}
+                    onChange={handleColorChange}
+                    checked={color === clr.name} 
+                  />
                   <span className="checkmark">✔</span>
                 </label>
-                <label className="color-label" style={{ backgroundColor: "#ba68c8" }}>
-                  <input type="radio" name="color" value="purple" />
-                  <span className="checkmark">✔</span>
-                </label>
-                <label className="color-label" style={{ backgroundColor: "#ffd44f" }}>
-                  <input type="radio" name="color" value="yellow" />
-                  <span className="checkmark">✔</span>
-                </label>
-                <label className="color-label" style={{ backgroundColor: "#56c4f4" }}>
-                  <input type="radio" name="color" value="blue" />
-                  <span className="checkmark">✔</span>
-                </label>
-                <label className="color-label" style={{ backgroundColor: "#aed581" }}>
-                  <input type="radio" name="color" value="green" />
-                  <span className="checkmark">✔</span>
-                </label>
-              </div>
-              <button>Add</button>
+              ))}
+            </div>
+   
+              <button onClick={handleAddNote}>Add</button>
             </div>
           </div>
         </div>
--- Week-10/NoteApp/src/context/NoteContext.jsx
+++ Week-10/NoteApp/src/context/NoteContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import PropTypes from 'prop-types'
 
 const NoteContext = createContext();
@@ -8,9 +8,52 @@ export const NoteProvider = ({ children }) => {
         children: PropTypes.node.isRequired
     }
 
+    const colors = [
+        { name: "pink", hex: "#f36795" },
+        { name: "purple", hex: "#ba68c8" },
+        { name: "yellow", hex: "#ffd44f" },
+        { name: "blue", hex: "#56c4f4" },
+        { name: "green", hex: "#aed581" },
+    ]
+
+    const [notes, setNotes] = useState([]); // Notlar
+    const [note, setNote] = useState(""); // Not
+    const [color, setColor] = useState("pink"); // Renk
 
 
-    const data = {}
+    const handleChange = (e) => {
+        const newNote = e.target.value;
+        setNote(newNote);
+        console.log(note);
+    }
+
+    const handleColorChange = (e) => {
+        const newColor = e.target.value;
+        setColor(newColor);
+        console.log(e.target.value);
+    }
+
+    const handleAddNote = () => {
+        if (note.trim() !== "") {
+            setNotes([...notes, { note, color}]);
+            setNote('');
+        } else {
+            console.log("Please enter a note");
+        }
+    }
+
+    useEffect(() => {   
+        
+    }, [notes])
+
+    const data = {
+        note,
+        color,
+        colors,
+        handleChange, 
+        handleColorChange, 
+        handleAddNote,
+    }
     return <NoteContext.Provider value={data}>{children}</NoteContext.Provider>
 }
 
